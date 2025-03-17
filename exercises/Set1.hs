92b107c7660fafcb8a44f771a75eda3f4258e485
jessbanana <siming.jml@gmail.com>
2025-03-17 12:47:19 +1100

    Update Set1.hs

--- exercises/Set1.hs
+++ exercises/Set1.hs
@@ -19,8 +19,9 @@ import Mooc.Todo
 
 ------------------------------------------------------------------------------
 -- Ex 1: define variables one and two. They should have type Int and
--- values 1 and 2, respectively.
-
+-- values 1 and 2, respectively. 
+one :: Integer 
+two :: Integer
 
 ------------------------------------------------------------------------------
 -- Ex 2: define the function double of type Integer->Integer. Double
